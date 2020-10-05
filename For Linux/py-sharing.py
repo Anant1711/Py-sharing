60f9453898e31acedfbacf89b65f4405b1e777dd
Anant chauhan <63709820+Anant1711@users.noreply.github.com>
2020-10-06 01:37:32 +0530

    Add files via upload

---
+++ For Linux/py-sharing.py
@@ -0,0 +1,47 @@
+#-*- coding: utf-8 -*-
+import os
+import socket
+import qrcode
+
+class bcolors:
+    ENDC = '\033[95m'
+    nm = '\033[93m'
+
+
+
+print ("")
+print ("")
+print (" ███████████                                █████                           ███                     ")
+print ("░░███░░░░░███                              ░░███                           ░░░                      ")
+print (" ░███    ░███ █████ ████             █████  ░███████    ██████   ████████  ████  ████████    ███████")
+print (" ░██████████ ░░███ ░███  ██████████ ███░░   ░███░░███  ░░░░░███ ░░███░░███░░███ ░░███░░███  ███░░███")
+print (" ░███░░░░░░   ░███ ░███ ░░░░░░░░░░ ░░█████  ░███ ░███   ███████  ░███ ░░░  ░███  ░███ ░███ ░███ ░███")
+print (" ░███         ░███ ░███             ░░░░███ ░███ ░███  ███░░███  ░███      ░███  ░███ ░███ ░███ ░███")
+print (" █████        ░░███████             ██████  ████ █████░░████████ █████     █████ ████ █████░░███████")
+print ("░░░░░          ░░░░░███            ░░░░░░  ░░░░ ░░░░░  ░░░░░░░░ ░░░░░     ░░░░░ ░░░░ ░░░░░  ░░░░░███")
+print ("               ███ ░███                                                                     ███ ░███")
+print ("              ░░██████                                                                     ░░██████ ")
+print ("               ░░░░░░                                                                       ░░░░░░  ")
+print ("                                                                                      " + bcolors.nm + "By Anant" + bcolors.ENDC)
+print ("")
+PORT=9004                     #Port on which our server will run (YOU can change)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print ("="*100)
+hostname = socket.gethostname()                 #it will tell us hostname
+print ("")
+print ("--------------------------------------[" + hostname + "]------------------------------------")
+print ("")
+
+print ("------{Enter this IP into your reciever device's browser as [IP:9004] or Scan the share.png}-------")
+print ("")
+
+s.connect(("8.8.8.8", PORT))
+print("-----------------------------------[" + s.getsockname()[0] + "]------------------------------------------")    #it will give us sender's IP
+print ("")
+
+img = qrcode.make(s.getsockname()[0]+":9004")                           #for store data in qrcode png 
+img.save("/home/infinity/Desktop/share.png")           #for saving qrcode png (change this path)
+
+os.system("python3 -m http.server 9004")             #it will start server at port 9002
