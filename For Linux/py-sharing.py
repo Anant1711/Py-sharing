104d5e0f47d692dcb91e8ceeed38ea2660a232e3
Anant chauhan <63709820+Anant1711@users.noreply.github.com>
2020-10-06 02:59:04 +0530

    Update py-sharing.py

--- For Linux/py-sharing.py
+++ For Linux/py-sharing.py
@@ -34,7 +34,7 @@ print ("")
 print ("--------------------------------------[" + hostname + "]------------------------------------")
 print ("")
 
-print ("------{Enter this IP into your reciever device's browser as [IP:9004] or Scan the share.png}-------")
+print ("------{Enter this IP into your receiver device's browser as [IP:9004] or Scan the share.png}-------")
 print ("")
 
 s.connect(("8.8.8.8", PORT))
